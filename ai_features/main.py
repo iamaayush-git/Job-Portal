02567db5e171540dcdfecebbdae9d975d202c3a4
Aayush Bhattarai <aayushbhattaraia6@gmail.com>
2025-11-22 16:11:42 +0545

    fix python file for deployment

--- ai_features/main.py
+++ ai_features/main.py
@@ -117,5 +117,5 @@ async def recommend(request:Request):
         raise HTTPException(status_code=500, detail=str(e))
     
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
