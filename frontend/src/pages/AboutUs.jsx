9c91e48b81fd01434c89c054795010d6d4fb14b9
Aayush Bhattarai <aayushbhattaraia6@gmail.com>
2025-11-22 10:29:37 +0545

    deployment test complete

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -9,7 +9,7 @@ const AboutUs = () => {
           About Us
         </h1>
         <p className="text-gray-600 max-w-3xl mx-auto text-lg md:text-xl">
-         Hello, new deployment. We connect talented professionals with their dream jobs and empower companies to build outstanding teams.
+          We connect talented professionals with their dream jobs and empower companies to build outstanding teams.
         </p>
       </section>
 
