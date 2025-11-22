909da6d0b4b1c3f019923ee54a9c8063fc74370c
Aayush Bhattarai <aayushbhattaraia6@gmail.com>
2025-11-22 10:55:58 +0545

    cors setting changed

--- backend/index.js
+++ backend/index.js
@@ -17,15 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.use(cors(
-  {
-    //  origin: 'http://localhost:5173',
-    origin: ['http://192.168.1.34:5173', 'http://localhost:5173'],
-    credentials: true,
-    // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    // allowedHeaders: ['Content-Type', 'Authorization']
-  }
-));
+app.use(cors());
 
 
 // api endpoints
